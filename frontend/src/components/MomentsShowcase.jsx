444139f95f1768bc60ed92f8003479c3cf7ad992
vishnuyarapu12 <vishnuyarapu6@gmail.com>
2026-07-02 11:49:47 +0530

    minor changes

--- frontend/src/components/MomentsShowcase.jsx
+++ frontend/src/components/MomentsShowcase.jsx
@@ -3,7 +3,7 @@ import { ChevronLeft, ChevronRight, Sparkles, RefreshCw } from 'lucide-react';
 import { momentImages } from '../utils/momentImages';
 import { pickRandomTitle, formatMomentLabel } from '../utils/momentsTitles';
 
-const DISPLAY_MS = 2000;
+const DISPLAY_MS = 1700;
 const TRANSITION_MS = 1000;
 
 const SlideLayer = React.memo(function SlideLayer({ slide, animClass, zIndex }) {
